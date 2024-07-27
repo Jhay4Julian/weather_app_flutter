4556f67ac966458cb0026f7d6303378646962985
Julian Koripamo <jkoripamo@gmail.com>
2024-07-27 09:03:00 +0100

    main and initial search screen

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController cityNameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Search bar
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: TextField(
+              controller: cityNameController,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.fromLTRB(8, 2, 8, 2),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+              ),
+            ),
+          ),
+          const SizedBox(height: 50),
+
+          // Search Button
+          SizedBox(
+            width: 120,
+            child: ElevatedButton(
+              style: const ButtonStyle(
+                padding: MaterialStatePropertyAll(
+                  EdgeInsets.all(20),
+                ),
+              ),
+              onPressed: () {},
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Search',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    // color: Colors.white,
+                    size: 20,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
