4d4524246ebbfd178cd09698ab11aa3be7fea13c
Julian Koripamo <jkoripamo@gmail.com>
2024-07-28 07:56:35 +0100

    implemented weather animation and added more properties to the temp card.

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app_flutter/models/weather_model.dart';
 import 'package:weather_app_flutter/services/weather_service.dart';
 
@@ -29,6 +30,31 @@ class _WeatherScreenState extends State<WeatherScreen> {
         _isLoading = false;
       });
       // Handle error
+    }
+  }
+
+  // weather animation function
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/animations/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'dust':
+      case 'fog':
+      case 'haze':
+      case 'mist':
+      case 'smoke':
+        return 'assets/animations/cloudy.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/animations/rain.json';
+      case 'thunderstorm':
+        return 'assets/animations/storm.json';
+      case 'clear':
+        return 'assets/animations/sunny.json';
+      default:
+        return 'assets/animations/sunny.json';
     }
   }
 
@@ -87,18 +113,30 @@ class _WeatherScreenState extends State<WeatherScreen> {
                         ),
                       ],
                     ),
+                    // Weather Card
                     TempCard(
+                      animation: getWeatherAnimation(_weather?.mainCondition),
+                      description: '${_weather?.description}',
                       temperature: '${_weather?.temperature.round()}°',
                       feelslike: 'feels like ${_weather?.feelsLike}°',
                     ),
-                    const SizedBox(height: 30),
-                    PropTile(title: 'Humidity', value: '${_weather?.humidity}'),
-                    PropTile(
-                        title: 'Visibility', value: '${_weather?.humidity}'),
-                    PropTile(
-                        title: 'Wind Speed', value: '${_weather?.windSpeed}'),
-                    PropTile(
-                        title: 'Wind Degree', value: '${_weather?.windDeg}'),
+
+                    // const SizedBox(height: 30),
+                    Column(
+                      children: [
+                        PropTile(
+                            title: 'Humidity', value: '${_weather?.humidity}'),
+                        PropTile(
+                            title: 'Visibility',
+                            value: '${_weather?.humidity}'),
+                        PropTile(
+                            title: 'Wind Speed',
+                            value: '${_weather?.windSpeed}'),
+                        PropTile(
+                            title: 'Wind Degree',
+                            value: '${_weather?.windDeg}'),
+                      ],
+                    )
                   ],
                 ),
         ),
@@ -110,12 +148,20 @@ class _WeatherScreenState extends State<WeatherScreen> {
 class TempCard extends StatelessWidget {
   final String temperature;
   final String feelslike;
-  const TempCard(
-      {super.key, required this.temperature, required this.feelslike});
+  final String animation;
+  final String description;
+  const TempCard({
+    super.key,
+    required this.temperature,
+    required this.feelslike,
+    required this.animation,
+    required this.description,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
       decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
@@ -125,16 +171,35 @@ class TempCard extends StatelessWidget {
           ),
           borderRadius: BorderRadius.all(Radius.circular(20))),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        child: Column(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              temperature,
-              style: const TextStyle(fontSize: 50, color: Colors.white),
+            Column(
+              children: [
+                Lottie.asset(
+                  animation,
+                  height: 100,
+                ),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 15, color: Colors.white),
+                ),
+              ],
             ),
-            Text(
-              feelslike,
-              style: const TextStyle(fontSize: 18, color: Colors.white),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  temperature,
+                  style: const TextStyle(fontSize: 50, color: Colors.white),
+                ),
+                Text(
+                  feelslike,
+                  style: const TextStyle(fontSize: 17, color: Colors.white),
+                ),
+              ],
             ),
           ],
         ),
