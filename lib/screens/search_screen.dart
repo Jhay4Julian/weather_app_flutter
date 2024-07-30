6e0a759da6052e8ca2540abbc4933e36ac57586a
Julian Koripamo <jkoripamo@gmail.com>
2024-07-30 15:03:32 +0100

    added animation

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app_flutter/screens/weather_screen.dart';
+import 'package:lottie/lottie.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -17,53 +18,59 @@ class _SearchScreenState extends State<SearchScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // Search bar
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: TextField(
-              controller: cityNameController,
-              decoration: const InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(8, 2, 8, 2),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-              ),
-            ),
-          ),
+          Lottie.asset('assets/animations/partly_cloudy.json'),
           const SizedBox(height: 50),
-
-          // Search Button
-          SizedBox(
-            width: 120,
-            child: ElevatedButton(
-              style: const ButtonStyle(
-                padding: MaterialStatePropertyAll(
-                  EdgeInsets.all(20),
+          // Search bar
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: TextFormField(
+                  controller: cityNameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter a city',
+                    contentPadding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
                 ),
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        WeatherScreen(cityName: cityNameController.text),
+              const SizedBox(height: 30),
+
+              // Search Button
+              SizedBox(
+                width: 120,
+                child: ElevatedButton(
+                  style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(
+                      EdgeInsets.all(20),
+                    ),
                   ),
-                );
-              },
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Search',
-                    style: TextStyle(fontSize: 17),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            WeatherScreen(cityName: cityNameController.text),
+                      ),
+                    );
+                  },
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Search',
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        size: 20,
+                      ),
+                    ],
                   ),
-                  Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    // color: Colors.white,
-                    size: 20,
-                  ),
-                ],
-              ),
-            ),
+                ),
+              )
+            ],
           )
         ],
       ),
